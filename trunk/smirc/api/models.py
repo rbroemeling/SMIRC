d9c82d5dfe572f6031f35b79e606942a1229d36b
Remi Broemeling <remi@broemeling.org>
2010-07-12 22:39:00 +0000

    Define an intermediary model so that we can track "voice" status of user<->room pairings.

--- trunk/smirc/api/models.py
+++ trunk/smirc/api/models.py
@@ -3,9 +3,19 @@ from django.contrib.auth.models import User
 
 class Room(models.Model):
 	name = models.CharField(max_length=16)
+	creator = models.ForeignKey(User)
+	users = models.ManyToManyField(User, related_name="rooms", through="Membership")
 
 	def __unicode__(self):
 		return self.name
+
+class Membership(models.Model):
+	user = models.ForeignKey(User)
+	room = models.ForeignKey(Room)
+	voice = models.BooleanField()
+
+	def __unicode__(self):
+		return "%s:%s" % (room.name, user.name)
 
 # Add a user profile to the Django User model so that we can
 # add on our own fields/user data as necessary.
@@ -13,7 +23,6 @@ class Room(models.Model):
 #    http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
 class UserProfile(models.Model):
 	phone_number = models.BigIntegerField(unique=True)
-	rooms = models.ManyToManyField(Room, related_name="users")
 	user = models.ForeignKey(User, unique=True)
 
 	def __unicode__(self):
