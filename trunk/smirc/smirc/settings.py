0d6c4ae7b85a8a4f8f2ef9185be34fb026c65a6a
Remi Broemeling <remi@broemeling.org>
2010-07-14 18:20:00 +0000

    Re-organize smirc settings.py file.

--- trunk/smirc/smirc/settings.py
+++ trunk/smirc/smirc/settings.py
@@ -1,14 +1,19 @@
-# Django settings for smirc project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
 
 ADMINS = (
 	('Remi Broemeling', 'remi@broemeling.org')
 	# ('Your Name', 'your_email@domain.com'),
 )
 
-MANAGERS = ADMINS
+# Tie our custom user profile class to the User.get_profile() method.
+# Technique taken from:
+#	http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
+AUTH_PROFILE_MODULE = 'api.UserProfile'
+
+DEBUG = True
 
 DATABASES = {
 	'default': {
@@ -21,28 +26,22 @@ DATABASES = {
 	}
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Edmonton'
+INSTALLED_APPS = (
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	# Uncomment the next line to enable the admin:
+	# 'django.contrib.admin',
+	'smirc.api'
+)
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
+MANAGERS = ADMINS
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
@@ -52,26 +51,6 @@ MEDIA_ROOT = ''
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Tie our custom user profile class to the User.get_profile() method.
-# Technique taken from:
-#	http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
-AUTH_PROFILE_MODULE = 'api.UserProfile'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '_b!mf0hketu4xt(l-j+ilw75*bh&xqt#25!v1(8rmv@l3q81wo'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.Loader',
-	'django.template.loaders.app_directories.Loader',
-#	'django.template.loaders.eggs.Loader',
-)
 
 MIDDLEWARE_CLASSES = (
 	'django.middleware.common.CommonMiddleware',
@@ -83,19 +62,39 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'smirc.urls'
 
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '_b!mf0hketu4xt(l-j+ilw75*bh&xqt#25!v1(8rmv@l3q81wo'
+
+SITE_ID = 1
+
+TEMPLATE_DEBUG = DEBUG
+
 TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
 )
 
-INSTALLED_APPS = (
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.sites',
-	'django.contrib.messages',
-	# Uncomment the next line to enable the admin:
-	# 'django.contrib.admin',
-	'smirc.api'
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+#	'django.template.loaders.eggs.Loader',
 )
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Edmonton'
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
