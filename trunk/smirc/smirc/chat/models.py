67cbc11ea08ada06c5e9dd94e51226a51f34d10b
Remi Broemeling <remi@broemeling.org>
2010-07-18 17:40:00 +0000

    Merge remaining smirc.api models into smirc.chat models.

---
+++ trunk/smirc/smirc/chat/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Membership(models.Model):
+	class Meta:
+		unique_together = (('user','room'))
+
+	user = models.ForeignKey(User)
+	room = models.ForeignKey(Room)
+	voice = models.BooleanField()
+
+	def __unicode__(self):
+		return '%s:%s' % (room.name, user.name)
+
+class Room(models.Model):
+	class Meta:
+		unique_together = (('owner','name'))
+
+	name = models.CharField(max_length=16, db_index=True)
+	owner = models.ForeignKey(User)
+	users = models.ManyToManyField(User, related_name='rooms', through='Membership')
+
+	def __unicode__(self):
+		return self.name
+
+# Add a user profile to the Django User model so that we can
+# add on our own fields/user data as necessary.
+# Technique taken from:
+#    http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
+class UserProfile(models.Model):
+	last_active_room = models.ForeignKey(Room)
+	phone_number = models.BigIntegerField(primary_key=True)
+	user = models.ForeignKey(User, unique=True)
+
+	def __unicode__(self):
+		return str(phone_number)
