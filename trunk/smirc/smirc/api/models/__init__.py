c4932851cc5f9b66244822fea0dcb48a9b64d40e
Remi Broemeling <remi@broemeling.org>
2010-07-18 16:29:05 +0000

    Add a bit of documentation about our app_label work-around.

--- trunk/smirc/smirc/api/models/__init__.py
+++ trunk/smirc/smirc/api/models/__init__.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# As a requirement of being used by Django, models defined inside a
+# models/ sub-directory need to be tagged with:
+# 	class Meta:
+#		app_label = 'foo'
+# For more information, see:
+# http://blog.amber.org/2009/01/19/moving-django-models-into-their-own-module/
+
 class Room(models.Model):
 	class Meta:
 		app_label = 'api'
