3668a8336da1d7c982b0fab2450b9fda2636a021
Remi Broemeling <remi@broemeling.org>
2010-07-29 22:11:02 +0000

    SMIRC-1 Add signal handling to smircd so that it shuts down (fairly) gracefully.

--- trunk/smirc/smircd.py
+++ trunk/smirc/smircd.py
@@ -6,16 +6,18 @@
 # on the django framework, it requires the DJANGO_SETTINGS_MODULE environment
 # variable to be set.
 #
-# In general, an example DJANGO_SETTINGS_MODULE environment variable setting will look
-# like this:
+# In general, an example DJANGO_SETTINGS_MODULE environment variable setting
+# will look like this:
 #
 #	export DJANGO_SETTINGS_MODULE="smirc.settings"
 #
 # See http://docs.djangoproject.com/en/dev/topics/settings/ for more information.
 #
+import inspect
 import logging
 import os
 import pyinotify
+import signal
 import sys
 from django.conf import settings
 from smirc.command.models import SmircCommand
@@ -54,6 +56,22 @@ class SMSFileHandler(pyinotify.ProcessEvent):
 		if response.body:
 			response.send(message.user.profile.phone_number)			
 
+def signal_handler(signum, frame):
+	global smircd_terminate
+	
+	sigdesc = 'UNKNOWN'
+	for member in inspect.getmembers(signal):
+		if member[0][0:3] == 'SIG' and member[1] == signum:
+			sigdesc = member[0]
+			break
+	if signum in [ signal.SIGINT, signal.SIGTERM, signal.SIGQUIT ]:
+		logging.info('signal_handler received signal %s(%d), setting terminate flag' % (sigdesc, signum))
+		smircd_terminate = True
+	elif signum in [ signal.SIGHUP ]:
+		logging.warning('signal_handler ignoring signal %s(%d)' % (sigdesc, signum))
+	else:
+		logging.error('signal_handler received unhandled signal %s(%d)' % (sigdesc, signum))
+
 def smircd_sanity_check():
 	errors = 0
 
@@ -84,13 +102,26 @@ def smircd_sanity_check():
 	if errors > 0:
 		sys.exit(-2)
 
+smircd_terminate = False
+
 if __name__ == '__main__':
 	logging.debug('settings.SMSTOOLS: %s', str(settings.SMSTOOLS))
 	smircd_sanity_check()
+
+	signal.signal(signal.SIGHUP, signal_handler)
+	signal.signal(signal.SIGINT, signal_handler)
+	signal.signal(signal.SIGTERM, signal_handler)
+	signal.signal(signal.SIGQUIT, signal_handler)
 
 	watch_manager = pyinotify.WatchManager()
 	sms_file_handler = SMSFileHandler()
 	notifier = pyinotify.Notifier(watch_manager, sms_file_handler)
 	watch_manager.add_watch(settings.SMSTOOLS['inbound_dir'], pyinotify.IN_CLOSE_WRITE)
 	
-	notifier.loop()
+	while True:
+		notifier.process_events()
+		if smircd_terminate == True:
+			notifier.stop()
+			break
+		if notifier.check_events():
+			notifier.read_events()
