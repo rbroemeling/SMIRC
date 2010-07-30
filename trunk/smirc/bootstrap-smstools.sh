ae7a238ac1550e85a6dd7dbab4e66bdaeae5b8c0
Remi Broemeling <remi@broemeling.org>
2010-07-30 06:41:03 +0000

    More details on making the BenQ M32 work, as well as smsd configuration for BenQ M32.

--- trunk/smirc/bootstrap-smstools.sh
+++ trunk/smirc/bootstrap-smstools.sh
@@ -18,21 +18,40 @@ logfile = /var/spool/sms/smsd.log
 loglevel = 7
 
 [GSM1]
-device = /dev/ttyS0
+device = /dev/ttyUSB0
+init = AT+CNMI=0,0,0,1,0
 incoming = yes
-#pin = 1111
+check_memory_method = 31
+phonecalls = clip
+voicecall_hangup_ath = yes
+voicecall_cpas = yes
+needs_wakeup_at = yes
+using_routed_status_report = yes
+routed_status_report_cnma = no
 ___EOF___
 __EOF__
 
 mkdir -p /var/spool/sms /var/spool/sms/checked /var/spool/sms/incoming /var/spool/sms/outgoing
 addgroup --system sms
 adduser --system --home /var/spool/sms --no-create-home --ingroup sms --disabled-password smsd
+addgroup smsd dialout
 chown -R smsd.sms /var/spool/sms
 chmod 0755 /var/spool/sms /var/spool/sms/checked
 chmod 0775 /var/spool/sms/incoming /var/spool/sms/outgoing
-echo
-echo ------------------------------------------------------------------------------------------
-echo Now run smsd with a command like this:
-echo   sudo -u smsd /usr/local/bin/smsd -c/usr/local/etc/smsd.conf -p/var/spool/sms/smsd.pid -s
-echo ------------------------------------------------------------------------------------------
-echo
+cat <<'__EOF__'
+
+------------------------------------------------------------------------------------------
+Now run smsd with a command like this:
+  sudo -u smsd /usr/local/bin/smsd -c/usr/local/etc/smsd.conf -p/var/spool/sms/smsd.pid -s
+------------------------------------------------------------------------------------------
+
+If using a BenQ M32 GPRS modem ( http://www.dealextreme.com/details.dx/sku.12057 ) and it
+is not being detected by linux (and thus not being assigned to /dev/ttyUSB0), try to use
+the pl2303 driver and tell it about the new USB identifier used by the BenQ M32,
+as seen below.
+------------------------------------------------------------------------------------------
+modprobe pl2303
+echo 067b 0609 > /sys/bus/usb-serial/drivers/pl2303/new_id
+------------------------------------------------------------------------------------------
+
+__EOF__
