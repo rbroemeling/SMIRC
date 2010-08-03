ff4f3aa754147d94bcb2fa34713c288f5001e099
Remi Broemeling <remi@broemeling.org>
2010-08-03 16:10:04 +0000

    Fix forwarding of messages -- we were improperly filtering on conversation=Membership

--- trunk/smirc/smircd.py
+++ trunk/smirc/smircd.py
@@ -59,7 +59,7 @@ class SMSFileHandler(pyinotify.ProcessEvent):
 				message.sender.last_active = datetime.datetime.utcnow()
 				message.sender.save()
 				try:
-					for recipient in Membership.objects.filter(conversation=message.sender).exclude(user=message.sender.user):
+					for recipient in Membership.objects.filter(conversation=message.sender.conversation).exclude(user=message.sender.user):
 						message.send(recipient.user.get_profile().phone_number)
 				except Membership.DoesNotExist:
 					pass		
