a6f84d6b230963003823425eeca71db095896462
Remi Broemeling <remi@broemeling.org>
2010-08-11 21:58:05 +0000

    Add a URL that can be used to reliably trigger a 500 error code in the even that we want to test our "Internal Server Error" page.

--- trunk/smirc/smirc/www/urls.py
+++ trunk/smirc/smirc/www/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
-	(r'^$', 'smirc.www.views.index'),     # No arguments, display the main homepage.
-	(r'^faq/$', 'smirc.www.views.faq'),   # Display our list of frequently-asked questions.
-	(r'^help/$', 'smirc.www.views.help')  # Display help documentation.
+	(r'^$', 'smirc.www.views.index'),          # No arguments, display the main homepage.
+	(r'^500$', 'smirc.www.views.nonexistent'), # Test our "Internal Server Error" page by purposefully causing an internal server error.
+	(r'^faq/$', 'smirc.www.views.faq'),        # Display our list of frequently-asked questions.
+	(r'^help/$', 'smirc.www.views.help')       # Display help documentation.
 )
