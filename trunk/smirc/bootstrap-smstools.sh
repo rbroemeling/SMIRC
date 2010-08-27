a9dccf47dc175cda79d01e06ed11adb0eaff647d
Remi Broemeling <remi@broemeling.org>
2010-08-27 16:11:00 +0000

    Update SMSTools3 configuration file to use Wavecom Fastrack GO on ttyACM0 rather than the M32 on ttyUSB0.

--- trunk/smirc/bootstrap-smstools.sh
+++ trunk/smirc/bootstrap-smstools.sh
@@ -12,7 +12,7 @@ sudo -u nobody mkdir "${STOW_DIR}/bin"
 sudo -u nobody make install BINDIR="${STOW_DIR}/bin"
 sudo -u nobody mkdir "${STOW_DIR}/etc" 
 cat >"${STOW_DIR}/etc/smsd.conf" <<___EOF___
-devices = GSM1
+devices = ACM0
 failed = /var/spool/sms/outgoing/failed
 infofile = /var/spool/sms/smsd.running
 logfile = /var/spool/sms/smsd.log
@@ -20,18 +20,13 @@ loglevel = 7
 phonecalls = /tmp
 report = /var/spool/sms/outgoing/report
 
-[GSM1]
-check_memory_method = 31
-device = /dev/ttyUSB0
-incoming = yes
-init = AT+CNMI=0,0,0,1,0
-needs_wakeup_at = yes
-phonecalls = clip
+[ACM0]
+device = /dev/ttyACM0
+incoming = high
+phonecalls = no
+phonecalls_purge = yes
 report = yes
-routed_status_report_cnma = no
-using_routed_status_report = yes
-voicecall_cpas = yes
-voicecall_hangup_ath = yes
+report_device_details = yes
 ___EOF___
 __EOF__
 
@@ -47,15 +42,6 @@ cat <<'__EOF__'
 ------------------------------------------------------------------------------------------
 Now run smsd with a command like this:
   sudo -u smsd /usr/local/bin/smsd -c/usr/local/etc/smsd.conf -p/var/spool/sms/smsd.pid -s
-------------------------------------------------------------------------------------------
-
-If using a BenQ M32 GPRS modem ( http://www.dealextreme.com/details.dx/sku.12057 ) and it
-is not being detected by linux (and thus not being assigned to /dev/ttyUSB0), try to use
-the pl2303 driver and tell it about the new USB identifier used by the BenQ M32,
-as seen below.
-------------------------------------------------------------------------------------------
-modprobe pl2303
-echo 067b 0609 > /sys/bus/usb-serial/drivers/pl2303/new_id
 ------------------------------------------------------------------------------------------
 
 __EOF__
